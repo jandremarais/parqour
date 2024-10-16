707d6d8a5e5f24740e1f4bccd34fa1e51f66ee62
Jan Marais <maraisjandre9@gmail.com>
2024-10-16 15:47:40 +0200

    add title line

--- src/tui/ui.rs
+++ src/tui/ui.rs
@@ -1,8 +1,37 @@
-use ratatui::{widgets::Paragraph, Frame};
+use ratatui::{
+    layout::{Constraint, Direction, Layout},
+    style::{Color, Style, Stylize},
+    text::Text,
+    widgets::{Block, Borders, Paragraph},
+    Frame,
+};
 
 use super::state::State;
 
 pub fn render(state: &mut State, frame: &mut Frame) {
-    let tmp = Paragraph::new(format!("PARQOUR!!: {}", state.viewer.filename()));
-    frame.render_widget(tmp, frame.area());
+    let screen = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(vec![Constraint::Length(1), Constraint::Min(1)])
+        .split(frame.area());
+
+    let title_line = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints(vec![Constraint::Percentage(50), Constraint::Percentage(50)])
+        .split(screen[0]);
+
+    frame.render_widget(
+        Paragraph::new(Text::styled(
+            " parqour ",
+            Style::default().bg(Color::LightCyan).fg(Color::DarkGray),
+        )),
+        title_line[0],
+    );
+    frame.render_widget(
+        Paragraph::new(Text::styled(
+            format!(" {} ", state.viewer.filename()),
+            Style::default().bg(Color::LightCyan).fg(Color::DarkGray),
+        ))
+        .right_aligned(),
+        title_line[1],
+    );
 }
